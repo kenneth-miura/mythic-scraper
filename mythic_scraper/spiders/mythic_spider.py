38829b119610504f2b4cbc455d7dfa954c0eb15a
kenneth-miura <kenneth.t.miura@gmail.com>
2019-04-26 05:55:30 -0400

    able to scrape and output each day's non-creatures
    Can only properly output a card with no power and toughness, i.e.
    anything but a creature

--- mythic_scraper/spiders/mythic_spider.py
+++ mythic_scraper/spiders/mythic_spider.py
@@ -3,15 +3,20 @@ import scrapy
 
 class MythicSpider(scrapy.Spider):
     name = "mythic"
+    url_starter = "http://mythicspoiler.com/"
 
     def start_requests(self):
         urls = [
-            'http://mythicspoiler.com/war/cards/gideonblackblade.html'
+            self.url_starter + 'newspoilers.html'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        for href in get_daily_spoilers(response):
+            yield scrapy.Request(url=self.url_starter + href, callback=self.parse_card)
+
+    def parse_card(self, response):
         print_formatted_card(response)
         pass
 
@@ -21,7 +26,7 @@ def print_formatted_card(response):
     print(get_card_cost(response))
     print(get_card_type(response))
     print(get_card_text(response))
-    print('\n _______________________________________________________________________________________________________ \n')
+    print('\n ______________________________________________________________ \n')
 
 
 def get_card_title(response):
@@ -38,3 +43,8 @@ def get_card_text(response):
 
 def get_card_cost(response):
     return response.xpath('//td[@colspan="2" and @valign="top"]/text()').getall()[1].strip()
+
+
+def get_daily_spoilers(response):
+    return response.xpath(
+        '//tr[count(preceding-sibling::tr[contains(comment()[2], "DATE BREAKER")])=1]/td/a[contains(@href, "html")]/@href').getall()
